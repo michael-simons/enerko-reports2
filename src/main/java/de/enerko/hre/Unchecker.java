ef0b5584b97f3d8550864008727ac34b0a49bc62
Michael J. Simons <michael@simons.ac>
2013-06-18 10:54:56 +0200

    Exception zu Throwable geändert

--- src/main/java/de/enerko/hre/Unchecker.java
+++ src/main/java/de/enerko/hre/Unchecker.java
@@ -9,7 +9,7 @@ public final class Unchecker {
 
 	// Now this returns an exception, so that you can satisfy the compiler by throwing it.
 	// But in reality we throw the given exception!
-	public static RuntimeException uncheck(final Exception ex){
+	public static RuntimeException uncheck(final Throwable ex){
 		// Now we use the 'generic' method. Normally the type T is inferred
 		// from the parameters. However you can specify the type also explicit!
 		// Now we du just that! We use the RuntimeException as type!
@@ -25,7 +25,7 @@ public final class Unchecker {
 	 * Remember, Generics are erased in Java. So this basically throws an Exception. The real
 	 * Type of T is lost during the compilation
 	 */	
-	private static <T extends Exception> void throwsUnchecked(Exception toThrow) throws T{
+	private static <T extends Exception> void throwsUnchecked(Throwable toThrow) throws T{
 		// Since the type is erased, this cast actually does nothing!!!
 		// we can throw any exception
 		// Cannot use @SuppressWarnings("unchecked") due to Oracle Restrictions!
