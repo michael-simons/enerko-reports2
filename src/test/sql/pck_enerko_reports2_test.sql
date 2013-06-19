59e3bec4df04a005256d9a203da50717cf21d97d
Michael J. Simons <michael@simons.ac>
2013-06-19 13:00:49 +0200

    Added a noargs pipelined report, fixed idention

--- src/test/sql/pck_enerko_reports2_test.sql
+++ src/test/sql/pck_enerko_reports2_test.sql
@@ -1,148 +1,163 @@
 CREATE OR REPLACE PACKAGE pck_enerko_reports2_test AS
-	FUNCTION f_no_args RETURN NUMBER;
+    FUNCTION f_no_args RETURN NUMBER;
 	
-	PROCEDURE p_no_args;
+    PROCEDURE p_no_args;
 	
-	FUNCTION f_some_args(p_number IN NUMBER, p_date IN DATE, p_datetime IN TIMESTAMP, p_string IN  VARCHAR2) RETURN NUMBER;
+    FUNCTION f_some_args(p_number IN NUMBER, p_date IN DATE, p_datetime IN TIMESTAMP, p_string IN  VARCHAR2) RETURN NUMBER;
 	
-	PROCEDURE p_some_args (p_number IN NUMBER, p_date IN DATE, p_datetime IN TIMESTAMP, p_string IN  VARCHAR2);
+    PROCEDURE p_some_args (p_number IN NUMBER, p_date IN DATE, p_datetime IN TIMESTAMP, p_string IN  VARCHAR2);
 	
-	FUNCTION f_fb_report_source_test(num_rows NUMBER, p_test_date IN DATE, p_test_string IN VARCHAR2) RETURN table_of_er_cell_definitions pipelined;
+    FUNCTION f_fb_report_source_test(num_rows NUMBER, p_test_date IN DATE, p_test_string IN VARCHAR2) RETURN table_of_er_cell_definitions pipelined;
 	
-	FUNCTION f_all_features RETURN table_of_er_cell_definitions pipelined;
+    FUNCTION f_noarg_report RETURN table_of_er_cell_definitions pipelined;
+    
+    FUNCTION f_all_features RETURN table_of_er_cell_definitions pipelined;
 END;
 /
 
 sho err
 
 CREATE OR REPLACE PACKAGE BODY pck_enerko_reports2_test AS
-	FUNCTION f_no_args RETURN NUMBER IS
-	BEGIN
-		return -1;
-	END f_no_args;
+    FUNCTION f_no_args RETURN NUMBER IS
+    BEGIN
+        return -1;
+    END f_no_args;
 	
-	PROCEDURE p_no_args IS
-	BEGIN
-		null;
-	END;
+    PROCEDURE p_no_args IS
+    BEGIN
+        null;
+    END;
 	
-	FUNCTION f_some_args(p_number IN NUMBER, p_date IN DATE, p_datetime IN TIMESTAMP, p_string IN  VARCHAR2) RETURN NUMBER IS
-	BEGIN
-		return -1;
-	END f_some_args;
+    FUNCTION f_some_args(p_number IN NUMBER, p_date IN DATE, p_datetime IN TIMESTAMP, p_string IN  VARCHAR2) RETURN NUMBER IS
+    BEGIN
+        return -1;
+    END f_some_args;
 	
-	PROCEDURE p_some_args(p_number IN NUMBER, p_date IN DATE, p_datetime IN TIMESTAMP, p_string IN  VARCHAR2) IS
-	BEGIN
-		null;
-	END p_some_args;
+    PROCEDURE p_some_args(p_number IN NUMBER, p_date IN DATE, p_datetime IN TIMESTAMP, p_string IN  VARCHAR2) IS
+    BEGIN
+        null;
+    END p_some_args;
 	
-	FUNCTION f_fb_report_source_test(num_rows NUMBER, p_test_date IN DATE, p_test_string IN VARCHAR2) RETURN table_of_er_cell_definitions pipelined IS
-	BEGIN
-		FOR i IN 0 .. (num_rows - 1) LOOP
-    		pipe row(
-      			t_er_cell_definition(
-	        		'f_fb_report_source_test',
-	        		0,
-	        		i,
-	        		'string',
-	        		'Row number ' || i || ' ' || p_test_string
-      			)
-      		);
-      	END LOOP;
-      	RETURN; 
-	END f_fb_report_source_test;
+    FUNCTION f_noarg_report RETURN table_of_er_cell_definitions pipelined IS
+    BEGIN
+        pipe row(
+            t_er_cell_definition(
+                'f_fb_report_source_test',
+                0,
+                0,
+                'string',
+                'I had no arguments.'
+            )
+        );
+    END;
+    
+    FUNCTION f_fb_report_source_test(num_rows NUMBER, p_test_date IN DATE, p_test_string IN VARCHAR2) RETURN table_of_er_cell_definitions pipelined IS
+    BEGIN
+        FOR i IN 0 .. (num_rows - 1) LOOP
+            pipe row(
+                t_er_cell_definition(
+                    'f_fb_report_source_test',
+                    0,
+                    i,
+                    'string',
+                    'Row number ' || i || ' ' || p_test_string
+                )
+            );
+        END LOOP;
+        RETURN; 
+    END f_fb_report_source_test;
 	
-	FUNCTION f_all_features RETURN table_of_er_cell_definitions pipelined IS
-		r INTEGER;
-	BEGIN
-		r:=0;		
-		-- Datentypen
-		pipe row(t_er_cell_definition(
-			'datatypes', 0, r, 'string', 'Bezeichnung'
-		));
-		pipe row(t_er_cell_definition(
-			'datatypes', 1, r, 'string', 'Wert'
-		));
+    FUNCTION f_all_features RETURN table_of_er_cell_definitions pipelined IS
+        r INTEGER;
+    BEGIN
+        r:=0;		
+        -- Datentypen
+        pipe row(t_er_cell_definition(
+            'datatypes', 0, r, 'string', 'Bezeichnung'
+        ));
+        pipe row(t_er_cell_definition(
+            'datatypes', 1, r, 'string', 'Wert'
+        ));
 		
-		r:=r+1;
-		pipe row(t_er_cell_definition(
-			'datatypes', 0, r, 'string', 'string'
-		));
-		pipe row(t_er_cell_definition(
-			'datatypes', 1, r, 'string', 'beliebiger string wert'
-		));
+        r:=r+1;
+        pipe row(t_er_cell_definition(
+            'datatypes', 0, r, 'string', 'string'
+        ));
+        pipe row(t_er_cell_definition(
+            'datatypes', 1, r, 'string', 'beliebiger string wert'
+        ));
 		
-		r:=r+1;
-		pipe row(t_er_cell_definition(
-			'datatypes', 0, r, 'string', 'number'
-		));
-		pipe row(t_er_cell_definition(
-			'datatypes', 1, r, 'number', '42.23'
-		));
-		pipe row(t_er_cell_definition(
-			'datatypes', 2, r, 'string', 'Mit Formatierung'
-		));
-		pipe row(t_er_cell_definition(
-			'datatypes', 3, r, 'number', '42.23@@#0.000'
-		));
+        r:=r+1;
+        pipe row(t_er_cell_definition(
+            'datatypes', 0, r, 'string', 'number'
+        ));
+        pipe row(t_er_cell_definition(
+            'datatypes', 1, r, 'number', '42.23'
+        ));
+        pipe row(t_er_cell_definition(
+            'datatypes', 2, r, 'string', 'Mit Formatierung'
+        ));
+        pipe row(t_er_cell_definition(
+            'datatypes', 3, r, 'number', '42.23@@#0.000'
+        ));
 				
-		r:=r+1;
-		pipe row(t_er_cell_definition(
-			'datatypes', 0, r, 'string', 'date'
-		));
-		pipe row(t_er_cell_definition(
-			'datatypes', 1, r, 'date', '06.05.2013'
-		));
+        r:=r+1;
+        pipe row(t_er_cell_definition(
+            'datatypes', 0, r, 'string', 'date'
+        ));
+        pipe row(t_er_cell_definition(
+            'datatypes', 1, r, 'date', '06.05.2013'
+        ));
 		
-		r:=r+1;
-		pipe row(t_er_cell_definition(
-			'datatypes', 0, r, 'string', 'date'
-		));
-		pipe row(t_er_cell_definition(
-			'datatypes', 1, r, 'datetime', '06.05.2013 03:41'
-		));
+        r:=r+1;
+        pipe row(t_er_cell_definition(
+            'datatypes', 0, r, 'string', 'date'
+        ));
+        pipe row(t_er_cell_definition(
+            'datatypes', 1, r, 'datetime', '06.05.2013 03:41'
+        ));
 		
-		r:=r+1;
-		pipe row(t_er_cell_definition(
-			'datatypes', 0, r, 'string', 'formula'
-		));
-		pipe row(t_er_cell_definition(
-			'datatypes', 1, r, 'formula', 'SUM(1,2,3,4,5,6,7,8,9,10)'
-		));
+        r:=r+1;
+        pipe row(t_er_cell_definition(
+            'datatypes', 0, r, 'string', 'formula'
+        ));
+        pipe row(t_er_cell_definition(
+            'datatypes', 1, r, 'formula', 'SUM(1,2,3,4,5,6,7,8,9,10)'
+        ));
 		
-		-- Formatvorlagen
-		r:=1;
-		pipe row(t_er_cell_definition(
-			'Nutzung von Formatvorlagen', 0, r, 'string', 'A1'
-		));
-		pipe row(t_er_cell_definition(
-			'Nutzung von Formatvorlagen', 1, r, 'string; "Formatvorlagen" A1', 'beliebiger string wert'
-		));
+        -- Formatvorlagen
+        r:=1;
+        pipe row(t_er_cell_definition(
+            'Nutzung von Formatvorlagen', 0, r, 'string', 'A1'
+        ));
+        pipe row(t_er_cell_definition(
+            'Nutzung von Formatvorlagen', 1, r, 'string; "Formatvorlagen" A1', 'beliebiger string wert'
+        ));
 		
-		r:=r+1;
-		pipe row(t_er_cell_definition(
-			'Nutzung von Formatvorlagen', 0, r, 'string', 'A2'
-		));
-		pipe row(t_er_cell_definition(
-			'Nutzung von Formatvorlagen', 1, r, 'datetime; "Formatvorlagen" A2', '06.05.2013 03:41'
-		));		
+        r:=r+1;
+        pipe row(t_er_cell_definition(
+            'Nutzung von Formatvorlagen', 0, r, 'string', 'A2'
+        ));
+        pipe row(t_er_cell_definition(
+            'Nutzung von Formatvorlagen', 1, r, 'datetime; "Formatvorlagen" A2', '06.05.2013 03:41'
+        ));		
 		
-		r:=r+1;
-		pipe row(t_er_cell_definition(
-			'Nutzung von Formatvorlagen', 0, r, 'string', 'A3'
-		));
-		pipe row(t_er_cell_definition(
-			'Nutzung von Formatvorlagen', 1, r, 'number; "Formatvorlagen" A3', '-4223.123'
-		));
+        r:=r+1;
+        pipe row(t_er_cell_definition(
+            'Nutzung von Formatvorlagen', 0, r, 'string', 'A3'
+        ));
+        pipe row(t_er_cell_definition(
+            'Nutzung von Formatvorlagen', 1, r, 'number; "Formatvorlagen" A3', '-4223.123'
+        ));
 		
-		FOR i IN 0 .. 19 LOOP
-		pipe row(t_er_cell_definition(
-			'diagramme', 0, i, 'formula', 'EXP(' || i || ')'
-		));
-		END LOOP;
+        FOR i IN 0 .. 19 LOOP
+            pipe row(t_er_cell_definition(
+                'diagramme', 0, i, 'formula', 'EXP(' || i || ')'
+            ));
+        END LOOP;
 		
-		RETURN;
-	END f_all_features;		
+        RETURN;
+    END f_all_features;		
 END;
 /
 
@@ -150,34 +165,34 @@ sho err
 
 CREATE OR REPLACE FUNCTION f_arg_resolver_test_no_args RETURN NUMBER AS 
 BEGIN
-	return -1;
+    return -1;
 END;
 /
 
 CREATE OR REPLACE PROCEDURE p_arg_resolver_test_no_args AS 
 BEGIN
-	NULL;
+    NULL;
 END;
 /
 
 CREATE OR REPLACE FUNCTION f_arg_resolver_test_some_args(
-	p_number IN NUMBER,
-	p_date IN DATE,
-	p_datetime IN TIMESTAMP,
-	p_string IN  VARCHAR2
+    p_number IN NUMBER,
+    p_date IN DATE,
+    p_datetime IN TIMESTAMP,
+    p_string IN  VARCHAR2
 ) RETURN NUMBER AS 
 BEGIN
-	return -1;
+    return -1;
 END;
 /
 
 CREATE OR REPLACE PROCEDURE p_arg_resolver_test_some_args(
-	p_number IN NUMBER,
-	p_date IN DATE,
-	p_datetime IN TIMESTAMP,
-	p_string IN  VARCHAR2
+    p_number IN NUMBER,
+    p_date IN DATE,
+    p_datetime IN TIMESTAMP,
+    p_string IN  VARCHAR2
 ) AS 
 BEGIN
-	NULL;
+    NULL;
 END;
 /
